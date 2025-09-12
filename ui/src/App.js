f9223b79c6dd31a54f19d346f6c813782b5a12a1
Jaybe-123456 <165712944+Jaybe-123456@users.noreply.github.com>
2025-09-12 23:09:59 +0300

    Create App.js

---
+++ ui/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ChatBox from "./components/ChatBox";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="app">
+      <ChatBox />
+    </div>
+  );
+}
+
+export default App;
